40cca4cbdaf4ef3a659041369ef5a122ed5398e9
israel <z4tur.pc@gmail.com>
2024-07-15 22:10:38 -0600

    Init

---
+++ src/main/java/com/aluraisrael/forohubisrael/ForohubisraelApplication.java
@@ -0,0 +1,13 @@
+package com.aluraisrael.forohubisrael;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForohubisraelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForohubisraelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aluraisrael/forohubisrael/ForohubisraelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluraisrael.forohubisrael;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForohubisraelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
